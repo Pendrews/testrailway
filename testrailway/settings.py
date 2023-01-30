512f14e42425de551894b807e2cb83acc271593d
Pendrews <azoskanyn@gmail.com>
2023-01-30 21:32:57 +0000

    added port 5000

--- testrailway/settings.py
+++ testrailway/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+port = int(os.environ.get('PORT', 5000))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
