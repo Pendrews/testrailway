a36ac8e1b9767fc69614f6a4dd52fa10f448aad1
Pendrews <azoskanyn@gmail.com>
2023-01-31 12:09:44 +0000

    added csrf_trusted_origins to settings.py

--- testrailway/settings.py
+++ testrailway/settings.py
@@ -132,3 +132,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-c3d7.up.railway.app/']
